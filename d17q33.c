a967a2dc6c43df63c4d81fcb8a6cbfbed86942cd
Srishti Sharma <ssrishti094@gmail.com>
2026-09-21 22:04:18 +0530

    Create d17q33.c

---
+++ d17q33.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, original, digit, sum = 0;
+
+    scanf("%d", &n);
+
+    original = n;
+
+    while (n != 0) {
+        digit = n % 10;
+        sum = sum + digit * digit * digit;
+        n = n / 10;
+    }
+
+    if (original == sum) {
+        printf("Armstrong");
+    }
+    else {
+        printf("Not Armstrong");
+    }
+
+    return 0;
+}
