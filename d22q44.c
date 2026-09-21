7773e5118edd1753b5f8ec40c8a771500682e721
Srishti Sharma <ssrishti094@gmail.com>
2026-09-21 22:32:03 +0530

    Create d22q44.c

---
+++ d22q44.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    float sum = 1.0;
+
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i++) {
+        sum = sum + (float)(2 * i - 1) / (2 * i);
+    }
+
+    printf("Approximate sum: %.1f", sum);
+
+    return 0;
+}
