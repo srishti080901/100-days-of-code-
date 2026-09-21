825e96eb7921509de8f5706782e957627abe6935
Srishti Sharma <ssrishti094@gmail.com>
2026-09-21 22:01:05 +0530

    Create d15q30.c

---
+++ d15q30.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, digit, reverse = 0;
+
+    scanf("%d", &n);
+
+    while (n != 0) {
+        digit = n % 10;
+        reverse = reverse * 10 + digit;
+        n = n / 10;
+    }
+
+    printf("%d", reverse);
+
+    return 0;
+}
