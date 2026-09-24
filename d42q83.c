50cdd4feff7aaf365f8e9941211eaa9e10c4bc0f
Srishti Sharma <ssrishti094@gmail.com>
2026-09-25 04:09:11 +0530

    Add vowel and consonant counting program

---
+++ d42q83.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int vowels = 0, consonants = 0;
+
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+
+        if (str[i] == 'a' || str[i] == 'e' ||
+            str[i] == 'i' || str[i] == 'o' ||
+            str[i] == 'u' || str[i] == 'A' ||
+            str[i] == 'E' || str[i] == 'I' ||
+            str[i] == 'O' || str[i] == 'U') {
+            
+            vowels++;
+        }
+
+        else if ((str[i] >= 'a' && str[i] <= 'z') ||
+                 (str[i] >= 'A' && str[i] <= 'Z')) {
+            
+            consonants++;
+        }
+    }
+
+    printf("Vowels=%d, Consonants=%d", vowels, consonants);
+
+    return 0;
+}
