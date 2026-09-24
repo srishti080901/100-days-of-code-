9b61da09b7f763cf3ca75cc96c44b7f750cc9e47
Srishti Sharma <ssrishti094@gmail.com>
2026-09-25 04:19:18 +0530

    Add character counting program in d45q89.c

---
+++ d45q89.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char str[100], ch;
+    int count = 0;
+
+    fgets(str, sizeof(str), stdin);
+
+    scanf("%c", &ch);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ch) {
+            count++;
+        }
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
