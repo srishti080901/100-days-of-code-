788646d0afabffb391c6eb09ffe851de52a86545
Srishti Sharma <ssrishti094@gmail.com>
2026-09-25 04:06:15 +0530

    Add character count program in d41q81.c
    Implement a program to count characters in input string.

---
+++ d41q81.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int count = 0;
+    fgets(str, sizeof(str), stdin);
+
+    while (str[count] != '\0' && str[count] != '\n') {
+        count++;
+    }
+     printf("%d", count);
+
+    return 0;
+}
