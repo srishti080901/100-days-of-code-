f38cc00657a89d350fa78503310ed83c3090e03e
Srishti Sharma <ssrishti094@gmail.com>
2026-09-24 13:41:20 +0530

    Add program to read and print an array of integers

---
+++ d28q56.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int arr[100];
+  // Read number of elements
+    scanf("%d", &n);
+ // Read array elements
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+  // Print array elements
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+  return 0;
+}
