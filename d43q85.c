09e1f320b6f2d37bce15667cf86505b1c0b621c2
Srishti Sharma <ssrishti094@gmail.com>
2026-09-25 04:12:47 +0530

    Create d43q85.c

---
+++ d43q85.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int length = 0;
+
+    fgets(str, sizeof(str), stdin);
+
+    while (str[length] != '\0' && str[length] != '\n') {
+        length++;
+    }
+
+    for (int i = length - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+
+    return 0;
+}
